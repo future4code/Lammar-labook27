438936932f192da277bf6dd2bc3f8aa6e6df46eb
gans92 <gabri.alisson.23@gmail.com>
2023-01-27 09:02:50 -0500

    fix the server

--- src/connection/BaseDatabase.ts
+++ src/connection/BaseDatabase.ts
@@ -9,8 +9,8 @@ export class Database {
        connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_SCHEMA,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         port: 3306
      }
     })
