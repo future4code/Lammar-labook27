6df56209086ea6776cc8f60fd2edc3c4e65775de
gans92 <gabri.alisson.23@gmail.com>
2023-01-30 11:32:49 -0500

    move the adress

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { app } from "./app"
 import { userRouter, postRouter } from "./routes/routes"
+import { AddressInfo } from "net"
 
 app.use('/user', userRouter)
 app.use('/post', postRouter)
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+ });
